de5fdb58b93a4eb8897a8630b7363bfc5f0b8b92
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-01-18 17:58:58 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,9 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext('2d');
+
+color = "red";
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200,200,40,0,2*Math.PI);
